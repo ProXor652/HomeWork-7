862b26ee14baeddf8fd2b6ee66d8c6d7d53cdcaf
ProXor652 <apmetalnikov@gmail.com>
2022-09-11 18:18:04 +0700

    Task 7 - 1

---
+++ Task7.cs
@@ -0,0 +1,36 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Enter number m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(-9,9);
+        }
+    }
+}
+int[,] Matrix = new int[m, n];
+
+PrintArray(Matrix);
+FillArray(Matrix);
+Console.WriteLine();
+PrintArray(Matrix);
